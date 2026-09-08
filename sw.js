18ed42d931e5eede187adcfc5515f6c9985876be
ankit-ai <alliances.ankit@gmail.com>
2026-09-08 13:58:38 +0530

    Self-update an installed PWA; show the build in Account; make the recents remove button clearer

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "folio-v32";
+const CACHE = "folio-v33";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 
 self.addEventListener("install", e => {

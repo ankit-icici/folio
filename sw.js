6667f703fd559530b0bd0f32d89ead98f39c268b
ankit-ai <alliances.ankit@gmail.com>
2026-09-09 23:02:25 +0530

    ESOP: exercise price, sell qty and sell price inline in the table; only tax and workings behind the tap

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "folio-v44";
+const CACHE = "folio-v45";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 
 self.addEventListener("install", e => {

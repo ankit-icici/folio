171eed2b2a500aa2e9c4eba2213643901f29ef6a
ankit-ai <alliances.ankit@gmail.com>
2026-09-09 20:23:08 +0530

    ESOP: simpler screen - four figures, one line per grant, no side-scrolling tables

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "folio-v39";
+const CACHE = "folio-v40";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 
 self.addEventListener("install", e => {

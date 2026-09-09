a7785b41dc1f7b5f54cf5f68754d4c44d54308ce
ankit-ai <alliances.ankit@gmail.com>
2026-09-09 20:06:57 +0530

    ESOP tab: exercised holdings, live ICICI Bank value, and the 30/30/40 vesting pipeline

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "folio-v37";
+const CACHE = "folio-v38";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 
 self.addEventListener("install", e => {

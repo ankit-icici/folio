9ed70c0ca55ae9747b805017be62836df6900816
ankit-ai <alliances.ankit@gmail.com>
2026-09-09 11:28:10 +0530

    Flows: financial-year and custom month-range periods instead of rolling durations

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "folio-v35";
+const CACHE = "folio-v36";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 
 self.addEventListener("install", e => {

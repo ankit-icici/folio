b3286859adc4b4ed4a76058e185a4190bd2c41d2
ankit-ai <alliances.ankit@gmail.com>
2026-09-09 20:15:26 +0530

    ESOP: 20% sale tax by default and per-lot editable; perquisite default 34%

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "folio-v38";
+const CACHE = "folio-v39";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 
 self.addEventListener("install", e => {

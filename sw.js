e40bd8b5d8f94eb84775c8ce8174185bea5a16f0
ankit-ai <alliances.ankit@gmail.com>
2026-09-10 17:00:37 +0530

    Drop the ex-HUF label from the funds hero

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "folio-v51";
+const CACHE = "folio-v52";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 
 self.addEventListener("install", e => {

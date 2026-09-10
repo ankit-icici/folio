8d49c71d7e259c01c7e06fd234232e4a373daff0
ankit-ai <alliances.ankit@gmail.com>
2026-09-10 14:36:05 +0530

    SWPs can name the fund they feed - how an SWP-fed SIP is really shaped

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "folio-v47";
+const CACHE = "folio-v48";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 
 self.addEventListener("install", e => {

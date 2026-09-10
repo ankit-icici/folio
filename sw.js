503ab0cc1afe0b9765f9c4a35e6c7b8872fb26fa
ankit-ai <alliances.ankit@gmail.com>
2026-09-10 16:26:59 +0530

    Funds: stock-style hero with ex-HUF absolute and CAGR, Portfolio/SIPs/SWPs/Lumpsum sub-tabs, per-fund ledger

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "folio-v49";
+const CACHE = "folio-v50";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 
 self.addEventListener("install", e => {
